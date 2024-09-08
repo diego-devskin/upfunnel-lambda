28605ccd4ce6132071fee75fd44242e489e1c581
root <root@Book-X50>
2024-09-08 17:34:31 -0300

    feat(): test deploy lambda

--- infra/data.tf
+++ infra/data.tf
@@ -1,11 +1,11 @@
 data "archive_file" "lambda_one" {
   type        = "zip"
-  source_file = "../code/${var.config.lambda_one_name}/index.js"
+  source_file = "../code/${var.config.lambda_one_name}/*"
   output_path = "${var.config.lambda_one_name}.zip"
 }
 
 data "archive_file" "lambda_two" {
   type        = "zip"
-  source_file = "../code/${var.config.lambda_two_name}/index.js"
+  source_file = "../code/${var.config.lambda_two_name}/*"
   output_path = "${var.config.lambda_two_name}.zip"
 }
