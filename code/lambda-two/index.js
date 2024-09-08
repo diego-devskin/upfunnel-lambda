ae81ee5a0d419139e77bb3468e1876d0b45aaed1
root <root@Book-X50>
2024-09-08 17:23:48 -0300

    feat(): test deploy lambda

--- code/lambda-two/index.js
+++ code/lambda-two/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello, World lambda two sdfsfsfds!'),
+        body: JSON.stringify('Hello, World lambda two sdfsdfsdaxx!'),
     };
     return response;
 };
